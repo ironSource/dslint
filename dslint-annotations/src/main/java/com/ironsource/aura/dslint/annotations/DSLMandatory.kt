55aabea4ba4077eb6580f637cdbe9896955bf754
hanan.rofe-haim <hanan.rofe-haim@ironsrc.com>
2020-08-15 17:32:01 +0300

    Added missing javadoc. Bumped version to alpha4.

--- dslint-annotations/src/main/java/com/ironsource/aura/dslint/annotations/DSLMandatory.kt
+++ dslint-annotations/src/main/java/com/ironsource/aura/dslint/annotations/DSLMandatory.kt
@@ -1,5 +1,11 @@
 package com.ironsource.aura.dslint.annotations
 
+/**
+ * DSLint library annotation.
+ * Use on mandatory DSL properties setters or functions. Properties/functions annotated by this will generate a lint error if not defined in the DSL lambda.
+ * @param group Optional group for property. DSLint will verify that at least one of the properties in the group is defined.
+ * @param message Optional custom error message. If not supplied a default message will be shown.
+ */
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.FUNCTION)
 annotation class DSLMandatory(val group: String = "", val message: String = "")
--- dslint-annotations/src/main/java/com/ironsource/aura/dslint/annotations/DSLint.kt
+++ dslint-annotations/src/main/java/com/ironsource/aura/dslint/annotations/DSLint.kt
@@ -1,5 +1,9 @@
 package com.ironsource.aura.dslint.annotations
 
+/**
+ * DSLint library annotation.
+ * Use on a DSL class/interface that you want DSLint to verify.
+ */
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.CLASS)
 @DslMarker

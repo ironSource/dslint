c6742d812c2ee4708ef901715a6b69e57f4b60a0
hanan.rofe-haim <hanan.rofe-haim@ironsrc.com>
2020-08-22 20:11:09 +0300

    Fixed cases where annotation attribute toUElement returns null

--- dslint-checks/src/main/java/com/ironsource/aura/dslint/utils/PsiAnnotationExtensions.kt
+++ dslint-checks/src/main/java/com/ironsource/aura/dslint/utils/PsiAnnotationExtensions.kt
@@ -1,13 +1,22 @@
 package com.ironsource.aura.dslint.utils
 
 import com.intellij.psi.PsiAnnotation
+import com.intellij.psi.PsiLiteral
 import org.jetbrains.uast.UExpression
 import org.jetbrains.uast.toUElement
 
 @Suppress("UNCHECKED_CAST")
 fun <T> PsiAnnotation?.resolveAttributeValue(
     attribute: String
-) = (this?.findAttributeValue(attribute).toUElement() as UExpression?)?.evaluate() as T?
+): T? {
+    val attributeValue = this?.findAttributeValue(attribute)
+    // Try to read as literal first
+    if (attributeValue is PsiLiteral) {
+        attributeValue.value?.let { return it as T }
+    }
+
+    return (attributeValue.toUElement() as UExpression?)?.evaluate() as T?
+}
 
 @Suppress("UNCHECKED_CAST")
 fun PsiAnnotation?.resolveStringAttributeValue(
